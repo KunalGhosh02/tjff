a6adbce23970f67c79f7ae7f2a2cc5e79bb9e947
mehrotra234 <aryanmehrotra2000@gmail.com>
2020-02-13 13:09:23 +0530

    EDITED day2

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,22 @@
-// TODO: In day 2 of workshop
+import 'package:flutter/material.dart';
+
+main() => runApp(Myapp());
+
+
+class Myapp extends StatelessWidget{
+@override 
+Widget build(BuildContext context){
+  return MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(title: Text("HellO World !!"),),
+      body: Column(
+        children: <Widget>[
+          Text("HEllo World !")
+        ],
+      ),
+
+    ),
+  );
+}
+
+}
